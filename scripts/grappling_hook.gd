10d5798591f3f2ecdb979099dc0dd3f41d59a382
Ostap <80210497+patsore@users.noreply.github.com>
2024-06-22 15:04:48 +0300

    trying to make a zipline using multimesh

--- scripts/grappling_hook.gd
+++ scripts/grappling_hook.gd
@@ -2,7 +2,6 @@ class_name GrapplingHook
 extends Node3D
 
 signal hook_attached(point_of_contact: Vector3)
-
 signal hook_detached
 
 var hook_target: Vector3
@@ -14,14 +13,15 @@ var shifted_positions: bool
 
 @export var hook_speed = 25.0
 @export var retract_speed = 10.0
-
 @export var max_distance = 100
-
 @export var initial_push_strength = 10.0
 
 @onready var hook_instance = MeshInstance3D.new()
-@onready var hook_path = $Path3D
-@onready var hook_mesh = $CSGPolygon
+
+# New nodes for the zipline
+var path: Path3D = Path3D.new()
+var mesh_instance: MultiMeshInstance3D = MultiMeshInstance3D.new()
+
 func _ready():
 	hook_instance.mesh = SphereMesh.new()
 	hook_instance.name = "GrapplingHook"
@@ -29,35 +29,49 @@ func _ready():
 	add_child(hook_instance)
 	hook_instance.visible = false
 	
-	hook_mesh.top_level = true
-	hook_mesh.visible = false
-	#hook_path.top_level = true
+	# Setup for the zipline
+	add_child(path)
+	path.name = "ZiplinePath"
+	
+	mesh_instance.multimesh = MultiMesh.new()
+	mesh_instance.multimesh.mesh = load("res://resources/zipline/zipline_mesh.tres")
+	mesh_instance.multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	add_child(mesh_instance)
+	mesh_instance.visible = false
 
-#Throws a grappling hook
-#that will attach to the first solid object it hits 
-#and pull you towards it.
+# Update the zipline visuals
+func update_zipline():
+	var start_position = head.global_transform.origin + Vector3(1, 0, 0)
+	var end_position = hook_instance.global_transform.origin
+	
+	var curve = Curve3D.new()
+	curve.add_point(start_position)
+	curve.add_point(end_position)
+	path.curve = curve
+	
+	var distance = start_position.distance_to(end_position)
+	var segments = int(distance)
+	mesh_instance.multimesh.instance_count = segments
+	for i in range(segments):
+		var t = float(i) / float(segments)
+		var transform = curve.sample_baked_with_rotation(t * distance)
+		mesh_instance.multimesh.set_instance_transform(i, transform)
+
 func shoot_grappling_hook():
-	# Define initial hook position and direction
 	hook_instance.global_transform.origin = head.global_transform.origin
 	var direction = -head.global_transform.basis.z
-	hook_instance.global_transform.origin += direction * 2.0  # Slightly offset in front of the character
+	hook_instance.global_transform.origin += direction * 2.0
 	
-	# Make the hook visible and start moving it
 	hook_instance.visible = true
 	hook_target = direction
-	
-	hook_mesh.visible = true
-	
 	is_attached = false
-	# Move the hook forward in the specified direction
 	set_physics_process(true)
 
 func _physics_process(delta):
 	if hook_instance.visible and not is_attached:
 		hook_instance.translate(hook_target * hook_speed * delta)
-		hook_mesh.global_transform.origin = global_transform.origin
-		hook_path.curve.set_point_position(1, hook_instance.global_transform.origin)
-		# Check for collision
+		update_zipline()
+		
 		var space_state = get_world_3d().direct_space_state
 		var query = PhysicsRayQueryParameters3D.create(hook_instance.global_transform.origin, hook_instance.global_transform.origin + hook_target * hook_speed * delta, 0x0000000F)
 		var result = space_state.intersect_ray(query)
@@ -66,6 +80,7 @@ func _physics_process(delta):
 			hook_attached.emit(result.position)
 	elif is_attached:
 		retract_grappling_hook()
+		update_zipline()
 
 func retract_grappling_hook():
 	var from = head.global_transform.origin
@@ -75,7 +90,7 @@ func retract_grappling_hook():
 	var query = PhysicsRayQueryParameters3D.create(from, to, 0x0000000F)
 	var result = space_state.intersect_ray(query)
 	
-	if result && (result.position - hook_target).length() > 1.0:
+	if result and (result.position - hook_target).length() > 1.0:
 		if shifted_positions:
 			print("disconnected here")
 			hook_detached.emit()
@@ -91,9 +106,6 @@ func retract_grappling_hook():
 		
 		parent.velocity += direction_to_hook * retract_speed * get_physics_process_delta_time()
 
-
-#When your hook connects to a surface, you will get a slight push forward towards the direction you are aiming.
-#Jumping just after the grappling hook reaches a surface will grant you a bit of extra momentum.
 func _on_hook_attached(point_of_contact: Vector3) -> void:
 	hook_target = point_of_contact
 	is_attached = true
@@ -101,10 +113,13 @@ func _on_hook_attached(point_of_contact: Vector3) -> void:
 	
 	var push_direction = (point_of_contact - global_transform.origin).normalized()
 	parent.velocity += push_direction * initial_push_strength * Vector3(1, 0.75, 1)
+	mesh_instance.visible = true
+	update_zipline()
 
 func _on_hook_detached() -> void:
 	is_attached = false
 	hook_instance.visible = false
+	mesh_instance.visible = false
 	set_physics_process(false)
 
 func _input(event: InputEvent) -> void:
